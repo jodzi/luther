d7b4d4097878e7411b498c6b761a47f14a15bca2
= <=>
2015-04-17 16:45:48 -0400

    fixing?

--- movie_scrape.py
+++ movie_scrape.py
@@ -18,25 +18,27 @@ def build_soup_page(url):
     soup = BeautifulSoup(page)
     return soup
     
+    
 def build_top_index(soup, to_search, beg_url):
     """
     builds an index from a soup object with a search string
     """
     index = []
-    
+      
     for a in soup.find_all('a', href=True):
         if a['href'].startswith(to_search):
             index.append(beg_url + a['href'])
     end_index = len(index) / 2
-    
+      
     return index[:end_index]
+    
     
 def build_sub_index(soup, search_one, search_two, beg_url):
     """
     builds an index from a soup object with a search and count string
     """
     index = []
-    
+      
     for a in soup.find_all('a', href=True):
         if a['href'].startswith(search_one) and re.search(search_two, a['href']) != None:
             index.append(beg_url + a['href'])
@@ -44,35 +46,30 @@ def build_sub_index(soup, search_one, search_two, beg_url):
     
     return index[:end_index]
     
+    
 def build_single_movie_url_list(total_urls, box_url):
     """
     builds a list of every movies url
     """
     single_movie_url_tags = []
 
-    #loops through each url and scrapes the page for all movies and appends
-    #the tags to the list    
+    #loops through each url and scrapes the page for all movies and appends the tags to the list    
     for full_site in total_urls:
-        soup = build_soup_page(full_site)
+        soup = build_soup_page(full_site)    
         for a in soup.find_all('a', href=True):
-            if re.search('id', a['href']) != None and a['href'] != '/movies/?id=fast7.htm':
+            if re.search('id', a['href']) != None and a['href'] not in single_movie_url_tags:
                 single_movie_url_tags.append(box_url + a['href'])
-    single_movie_url_tags.append(box_url + '/movies/?id=fast7.htm')
     
-    #gets rid of duplicate movies
-    single_movies_copy = single_movie_url_tags[:]
-    for el in single_movies_copy:
-        if single_movies_copy.count(el) > 1:
-            single_movie_url_tags.remove(el)
+    return set(single_movie_url_tags)
     
-    return single_movie_url_tags
     
 def main():
+    
     # creates a soup object containing the href for all pages A-Z & NUM
     top_level = build_soup_page("http://www.boxofficemojo.com/movies")
     # builds a list of each url ending for NUM and A-Z movie pages
     top_level_urls = build_top_index(top_level, 'alphabetical', "http://www.boxofficemojo.com/movies/")
-    
+       
     total_urls = top_level_urls[:]
 
     for top_level_url in top_level_urls:
@@ -80,11 +77,12 @@ def main():
         sub_level = build_sub_index(top_level_soup, '/movies/', 'page', "http://www.boxofficemojo.com")
         if len(sub_level) > 0:
             for sub_level_url in sub_level:
-                if sub_level_url.count('id') == 0:
+                if sub_level_url.count('id') == 0:                  
                     total_urls.append(sub_level_url)    
                     
     single_movies = build_single_movie_url_list(total_urls, "http://www.boxofficemojo.com")
     print sorted(single_movies)
+    
     
 if __name__ == '__main__':
     main()
